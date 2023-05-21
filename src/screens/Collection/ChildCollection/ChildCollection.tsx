8bfe1af4297bf53d80f0b6e8843709b5292bc9df
Khánh Nguyễn Quốc <khanhhzh@gmail.com>
2023-05-21 12:21:10 +0700

    feat: Add Collection file

---
+++ src/screens/Collection/ChildCollection/ChildCollection.tsx
@@ -0,0 +1,213 @@
+import React, {useState} from 'react';
+import {FlatList, Image, Text} from 'react-native';
+import styled from 'styled-components';
+import {useNavigation} from '@react-navigation/native';
+import {NativeBaseProvider} from 'native-base';
+import {
+  contentCollectionSelector,
+  titleNameSelector,
+} from '../../../Redux/Selectors';
+import {useDispatch, useSelector} from 'react-redux';
+import {contactSlide} from '../../Home/Contact/ContactSlide';
+import {contentCollectionSlide} from './ContentCollectionSlide';
+export default function ChildCollection() {
+  const title = useSelector(titleNameSelector);
+  const navigation = useNavigation();
+  const [editName, setStatusEditName] = useState(false);
+  const contentCollectionList = useSelector(contentCollectionSelector);
+  const [checkedList, setCheckedList] = useState([]);
+  const dispatch = useDispatch();
+  const detailContact = id => {
+    dispatch(contactSlide.actions.detailContactFrom(id));
+  };
+  const isChecked = id => {
+    return checkedList.includes(id);
+  };
+  const toggleStatus = id => {
+    if (editName === true) {
+      if (isChecked(id)) {
+        setCheckedList(checkedList.filter(item => item !== id));
+      } else {
+        setCheckedList([...checkedList, id]);
+      }
+    }
+  };
+  const deleteContactCollection = () => {
+    dispatch(
+      contentCollectionSlide.actions.deleteContactOfCollection(checkedList),
+    );
+  };
+  // const contentSelected = () => {
+  //   return contentCollectionList.map(item =>
+  //     Object.assign({}, item, {checked: false}),
+  //   );
+  // };
+  const Item = ({item}) => {
+    return (
+      <Per
+        onPress={() => {
+          detailContact(item.id);
+          toggleStatus(item.id);
+          editName === false ? navigation.navigate('Information') : {};
+        }}>
+        <Image source={item.imageAvata} />
+        <WrapInfor>
+          <Infor>
+            <Name> {item.fullName} </Name>
+            <Text> {item.phone} </Text>
+          </Infor>
+          {editName === true ? (
+            isChecked(item.id) ? (
+              <Image
+                source={require('../../../assets/icons/checkBoxTrue.png')}
+              />
+            ) : (
+              <Image
+                source={require('../../../assets/icons/checkBoxfalse.png')}
+              />
+            )
+          ) : (
+            <></>
+          )}
+        </WrapInfor>
+      </Per>
+    );
+  };
+  const renderItem = item => {
+    return <Item item={item.item} />;
+  };
+  return (
+    <NativeBaseProvider style={{flex: 1, backgourndColor: 'white'}}>
+      <Header>
+        <WrapHeaderTitle>
+          <HeaderTitle>{title}</HeaderTitle>
+          <Text>{contentCollectionList.length} thành viên</Text>
+        </WrapHeaderTitle>
+        <BackButton
+          onPress={() => {
+            navigation.navigate('Collection');
+            setStatusEditName(false);
+            setCheckedList([]);
+          }}>
+          <Image source={require('../../../assets/icons/arrow.png')} />
+        </BackButton>
+        {editName === true ? (
+          <DeleteButton
+            onPress={() => {
+              deleteContactCollection();
+              setCheckedList([]);
+            }}>
+            <Delete>Delete ({checkedList.length})</Delete>
+          </DeleteButton>
+        ) : (
+          ''
+        )}
+      </Header>
+
+      <FlatList
+        style={{backgroundColor: 'white'}}
+        data={contentCollectionList}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+
+      <BottomButton>
+        <ValueBottomButton
+          onPress={() => {
+            navigation.navigate('AddContact');
+            setStatusEditName(false);
+            setCheckedList([]);
+          }}>
+          <Image source={require('../../../assets/icons/Addcontact.png')} />
+          <TextBottomButton>Add contact</TextBottomButton>
+        </ValueBottomButton>
+        <ValueBottomButton
+          onPress={() => {
+            setStatusEditName(!editName);
+            setCheckedList([]);
+          }}>
+          <Image source={require('../../../assets/icons/editName.png')} />
+          <TextBottomButton>Edit name</TextBottomButton>
+        </ValueBottomButton>
+      </BottomButton>
+    </NativeBaseProvider>
+  );
+}
+const Header = styled.View`
+  background-color: white;
+  height: 54px;
+  flex-direction: row;
+  align-items: center;
+  border-bottom-width: 1px;
+  border-bottom-color: #bdbdbd;
+`;
+const BackButton = styled.TouchableOpacity`
+  position: absolute;
+  margin-left: 10px;
+`;
+const WrapHeaderTitle = styled.View`
+  flex: 1;
+  align-items: center;
+`;
+const HeaderTitle = styled.Text`
+  font-family: Roboto;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  color: #000;
+`;
+const DeleteButton = styled.TouchableOpacity`
+  position: absolute;
+  margin-left: 300px;
+`;
+const Delete = styled.Text`
+  font-family: Roboto;
+  font-style: normal;
+  font-size: 18px;
+  font-weight: 600;
+  color: #ff4a4a;
+`;
+const Per = styled.TouchableOpacity`
+  flex-direction: row;
+  margin-left: 15px;
+  height: 55px;
+  align-items: center;
+`;
+const WrapInfor = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  height: 100%;
+  margin-right: 35px;
+  margin-left: 15px;
+  border-bottom-width: 1px;
+  border-bottom-color: #e6e6e6;
+`;
+const Infor = styled.View`
+  justify-content: space-evenly;
+`;
+const Name = styled.Text`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  color: #000;
+`;
+const BottomButton = styled.View`
+  background-color: #1e62be;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  height: 60px;
+`;
+const TextBottomButton = styled.Text`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  color: white;
+`;
+const ValueBottomButton = styled.TouchableOpacity`
+  align-items: center;
+`;
---
+++ src/screens/Collection/Collection/CollectionSlide.tsx
@@ -0,0 +1,61 @@
+import {createSlice} from '@reduxjs/toolkit';
+export const collectionSlide = createSlice({
+  name: 'collectionList',
+  initialState: {
+    titleName: '',
+    collectionList: [
+      {
+        id: 1,
+        name: 'All',
+        folderBig: require('../../../assets/icons/iconFolderBig.png'),
+        moreOption: require('../../../assets/icons/moreOption.png'),
+      },
+      {
+        id: 2,
+        name: 'General',
+        folderBig: require('../../../assets/icons/iconFolderBig.png'),
+        moreOption: require('../../../assets/icons/moreOption.png'),
+      },
+      {
+        id: 3,
+        name: 'Investors',
+        folderBig: require('../../../assets/icons/iconFolderBig.png'),
+        moreOption: require('../../../assets/icons/moreOption.png'),
+      },
+      {
+        id: 4,
+        name: 'Lead',
+        folderBig: require('../../../assets/icons/iconFolderBig.png'),
+        moreOption: require('../../../assets/icons/moreOption.png'),
+      },
+      {
+        id: 5,
+        name: 'VIP',
+        folderBig: require('../../../assets/icons/iconFolderBig.png'),
+        moreOption: require('../../../assets/icons/moreOption.png'),
+      },
+    ],
+  },
+  reducers: {
+    addCollection: (state, action) => {
+      state.collectionList.push(action.payload);
+    },
+    editNameGroup: (state, action) => {
+      const newList = state.collectionList.find(
+        item => item.id === action.payload.idGroup,
+      );
+      if (newList) {
+        newList.name = action.payload.nameGroup;
+      }
+    },
+    deleteGroup: (state, action) => {
+      state.collectionList.splice(
+        state.collectionList.findIndex(item => item.id === action.payload),
+        1,
+      );
+    },
+    navigateTitleName: (state, action) => {
+      state.titleName = action.payload;
+    },
+  },
+});
